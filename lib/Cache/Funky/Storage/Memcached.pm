46f558db36fac7c5dbb88d0f65b636e2290e9d1a
Tomohiro Teranishi <tomyhero@cpan.org>
2007-10-14 02:59:57 +0000

    Import of TOMYHERO/Cache-Funky-Storage-Memcached-0.04 from CPAN.
    gitpan-cpan-distribution: Cache-Funky-Storage-Memcached
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         TOMYHERO/Cache-Funky-Storage-Memcached-0.04.tar.gz
    gitpan-cpan-author:       TOMYHERO
    gitpan-cpan-maturity:     released

--- lib/Cache/Funky/Storage/Memcached.pm
+++ lib/Cache/Funky/Storage/Memcached.pm
@@ -5,7 +5,7 @@ use strict;
 use base qw/Cache::Funky::Storage/;
 use Cache::Memcached;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 sub new {
     my $class = shift;
@@ -21,6 +21,9 @@ sub set {
     my $s     = shift;
     my $key   = shift;
     my $value = shift;
+    my $id    = shift;
+
+    $key = $id ? $key . ':' . $id : $key ;
 
     $s->{memcached}->set( $key , $value );
 
@@ -30,6 +33,9 @@ sub set {
 sub get {
     my $s     = shift;
     my $key   = shift;
+    my $id    = shift;
+
+    $key = $id ? $key . ':' . $id : $key ;
 
     return $s->{memcached}->get( $key );
 
@@ -38,6 +44,9 @@ sub get {
 sub delete {
     my $s     = shift;
     my $key   = shift;
+    my $id    = shift;
+
+    $key = $id ? $key . ':' . $id : $key ;
 
     $s->{memcached}->delete( $key );
 }
@@ -63,6 +72,7 @@ Memcached Storage for L<Cache::Funky>
   
   __PACKAGE__->setup('Storage::Memcached' => { servers => [ '127.0.0.1:12345' ] });
   __PACKAGE__->register( 'foo', sub { time } );
+  __PACKAGE__->register( 'boo', sub { shift . '_' . `date` } ); # * date: $id + _Tue May 1 21:53:36 JST 2007
   
   1;
 
@@ -77,8 +87,22 @@ run.pl
   sleep(3);
   say ( MyCache->foo() );
   
-  MyCache->delete(qw/foo/);
+  MyCache->delete('foo');
   say ( MyCache->foo() );
+
+  say ( MyCache->boo('id1') );
+  say ( MyCache->boo('id2') );
+    
+  sleep 10;
+  say ( MyCache->boo('id1') );
+    
+  # only remove id1
+  MyCache->delete('boo', 'id1');
+
+  say ( MyCache->boo('id1') );
+  say ( MyCache->boo('id2') );
+
+  MyCache->deletes([qw/foo boo/]);
 
 =head1 METHOD
 
